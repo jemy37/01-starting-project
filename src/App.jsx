98a23c58946157d591cea87de6e11e3cd58a2c69
jemy37 <gaojemy@gmail.com>
2025-01-14 00:27:59 +1100

    14/01/25 second update

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            {CORE_CONCEPTS.map((conceptItem) => <CoreConcept{...conceptItem} />)}
+            {CORE_CONCEPTS.map(conceptItem => <CoreConcept key={conceptItem.title}{...conceptItem} />)}
             {/* <CoreConcept 
             {...CORE_CONCEPTS[0]}/>
             <CoreConcept 
